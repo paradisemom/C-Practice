62b4576ba1b8a34dad1fedc1d49ff82be624ad10
paradisemom <andy774583558@gmail.com>
2024-12-09 19:27:04 +0800

    12/9

--- Book/7-3/WindowMgr.h
+++ Book/7-3/WindowMgr.h
@@ -12,7 +12,7 @@ class WindowMgr{
 public:
     using ScreenIndex=vector<Screen>::size_type;
     void Clear(ScreenIndex);
-    void Clear(Screen &);    // 新增操作任意 Screen 的方法
+    void Clear(Screen &);    // 新增操作任意 Screen 的方法  dasd
     ScreenIndex AddScreen(const Screen &);
     vector<Screen> screens;
 
